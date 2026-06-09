83c2ac461fe3f2bf4dc2efe0322699a43cbb3514
AntoniaDnv <antonia.d.neva@gmail.com>
2026-06-09 09:31:47 +0300

    Add LikeService and FollowService unit tests

---
+++ ChirpNet.Tests/Services/FollowServiceTests.cs
@@ -0,0 +1,224 @@
+﻿using ChirpNet.Data;
+using ChirpNet.Data.Data;
+using ChirpNet.Data.Data.Models;
+using ChirpNet.Services.Data.Services;
+using ChirpNet.Tests.Helpers;
+using NUnit.Framework;
+
+namespace ChirpNet.Tests.Services;
+
+[TestFixture]
+public class FollowServiceTests
+{
+    private ApplicationDbContext dbContext = null!;
+    private FollowService followService = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.dbContext = TestDbContextFactory.CreateDbContext();
+        this.followService = new FollowService(this.dbContext);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        this.dbContext.Dispose();
+    }
+
+    [Test]
+    public async Task FollowAsync_ShouldCreateFollow_WhenUsersAreValid()
+    {
+        ApplicationUser follower = new ApplicationUser
+        {
+            Id = "follower-id",
+            UserName = "follower@test.com",
+            Email = "follower@test.com",
+            DisplayName = "Follower"
+        };
+
+        ApplicationUser following = new ApplicationUser
+        {
+            Id = "following-id",
+            UserName = "following@test.com",
+            Email = "following@test.com",
+            DisplayName = "Following"
+        };
+
+        await this.dbContext.Users.AddRangeAsync(follower, following);
+        await this.dbContext.SaveChangesAsync();
+
+        bool result = await this.followService.FollowAsync(follower.Id, following.Id);
+
+        Assert.That(result, Is.True);
+        Assert.That(this.dbContext.Follows.Count(), Is.EqualTo(1));
+
+        Follow follow = this.dbContext.Follows.First();
+
+        Assert.That(follow.FollowerId, Is.EqualTo(follower.Id));
+        Assert.That(follow.FollowingId, Is.EqualTo(following.Id));
+    }
+
+    [Test]
+    public async Task FollowAsync_ShouldReturnFalse_WhenUserTriesToFollowSelf()
+    {
+        ApplicationUser user = new ApplicationUser
+        {
+            Id = "user-id",
+            UserName = "user@test.com",
+            Email = "user@test.com",
+            DisplayName = "User"
+        };
+
+        await this.dbContext.Users.AddAsync(user);
+        await this.dbContext.SaveChangesAsync();
+
+        bool result = await this.followService.FollowAsync(user.Id, user.Id);
+
+        Assert.That(result, Is.False);
+        Assert.That(this.dbContext.Follows.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task FollowAsync_ShouldReturnFalse_WhenFollowingUserDoesNotExist()
+    {
+        ApplicationUser follower = new ApplicationUser
+        {
+            Id = "follower-id",
+            UserName = "follower@test.com",
+            Email = "follower@test.com",
+            DisplayName = "Follower"
+        };
+
+        await this.dbContext.Users.AddAsync(follower);
+        await this.dbContext.SaveChangesAsync();
+
+        bool result = await this.followService.FollowAsync(follower.Id, "missing-user-id");
+
+        Assert.That(result, Is.False);
+        Assert.That(this.dbContext.Follows.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task FollowAsync_ShouldReturnFalse_WhenAlreadyFollowing()
+    {
+        ApplicationUser follower = new ApplicationUser
+        {
+            Id = "follower-id",
+            UserName = "follower@test.com",
+            Email = "follower@test.com",
+            DisplayName = "Follower"
+        };
+
+        ApplicationUser following = new ApplicationUser
+        {
+            Id = "following-id",
+            UserName = "following@test.com",
+            Email = "following@test.com",
+            DisplayName = "Following"
+        };
+
+        Follow existingFollow = new Follow
+        {
+            FollowerId = follower.Id,
+            FollowingId = following.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await this.dbContext.Users.AddRangeAsync(follower, following);
+        await this.dbContext.Follows.AddAsync(existingFollow);
+        await this.dbContext.SaveChangesAsync();
+
+        bool result = await this.followService.FollowAsync(follower.Id, following.Id);
+
+        Assert.That(result, Is.False);
+        Assert.That(this.dbContext.Follows.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task IsFollowingAsync_ShouldReturnTrue_WhenFollowExists()
+    {
+        ApplicationUser follower = new ApplicationUser
+        {
+            Id = "follower-id",
+            UserName = "follower@test.com",
+            Email = "follower@test.com",
+            DisplayName = "Follower"
+        };
+
+        ApplicationUser following = new ApplicationUser
+        {
+            Id = "following-id",
+            UserName = "following@test.com",
+            Email = "following@test.com",
+            DisplayName = "Following"
+        };
+
+        Follow follow = new Follow
+        {
+            FollowerId = follower.Id,
+            FollowingId = following.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await this.dbContext.Users.AddRangeAsync(follower, following);
+        await this.dbContext.Follows.AddAsync(follow);
+        await this.dbContext.SaveChangesAsync();
+
+        bool result = await this.followService.IsFollowingAsync(follower.Id, following.Id);
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public async Task IsFollowingAsync_ShouldReturnFalse_WhenFollowDoesNotExist()
+    {
+        bool result = await this.followService.IsFollowingAsync("user-1", "user-2");
+
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public async Task UnfollowAsync_ShouldRemoveFollow_WhenFollowExists()
+    {
+        ApplicationUser follower = new ApplicationUser
+        {
+            Id = "follower-id",
+            UserName = "follower@test.com",
+            Email = "follower@test.com",
+            DisplayName = "Follower"
+        };
+
+        ApplicationUser following = new ApplicationUser
+        {
+            Id = "following-id",
+            UserName = "following@test.com",
+            Email = "following@test.com",
+            DisplayName = "Following"
+        };
+
+        Follow follow = new Follow
+        {
+            FollowerId = follower.Id,
+            FollowingId = following.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await this.dbContext.Users.AddRangeAsync(follower, following);
+        await this.dbContext.Follows.AddAsync(follow);
+        await this.dbContext.SaveChangesAsync();
+
+        bool result = await this.followService.UnfollowAsync(follower.Id, following.Id);
+
+        Assert.That(result, Is.True);
+        Assert.That(this.dbContext.Follows.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task UnfollowAsync_ShouldReturnFalse_WhenFollowDoesNotExist()
+    {
+        bool result = await this.followService.UnfollowAsync("user-1", "user-2");
+
+        Assert.That(result, Is.False);
+    }
+}
---
+++ ChirpNet.Tests/Services/LikeServiceTests.cs
@@ -0,0 +1,180 @@
+﻿using ChirpNet.Data;
+using ChirpNet.Data.Data;
+using ChirpNet.Data.Data.Models;
+using ChirpNet.Services.Data.Services;
+using ChirpNet.Tests.Helpers;
+using NUnit.Framework;
+
+namespace ChirpNet.Tests.Services;
+
+[TestFixture]
+public class LikeServiceTests
+{
+    private ApplicationDbContext dbContext = null!;
+    private LikeService likeService = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.dbContext = TestDbContextFactory.CreateDbContext();
+        this.likeService = new LikeService(this.dbContext);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        this.dbContext.Dispose();
+    }
+
+    [Test]
+    public async Task ToggleLikeAsync_ShouldLikePost_WhenPostIsNotLiked()
+    {
+        ApplicationUser user = new ApplicationUser
+        {
+            Id = "user-1",
+            UserName = "user@test.com",
+            Email = "user@test.com",
+            DisplayName = "Test User"
+        };
+
+        Post post = new Post
+        {
+            Id = 1,
+            Content = "Test post",
+            AuthorId = user.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await this.dbContext.Users.AddAsync(user);
+        await this.dbContext.Posts.AddAsync(post);
+        await this.dbContext.SaveChangesAsync();
+
+        var result = await this.likeService.ToggleLikeAsync(post.Id, user.Id);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result!.IsLiked, Is.True);
+        Assert.That(result.LikesCount, Is.EqualTo(1));
+        Assert.That(this.dbContext.Likes.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task ToggleLikeAsync_ShouldUnlikePost_WhenPostIsAlreadyLiked()
+    {
+        ApplicationUser user = new ApplicationUser
+        {
+            Id = "user-1",
+            UserName = "user@test.com",
+            Email = "user@test.com",
+            DisplayName = "Test User"
+        };
+
+        Post post = new Post
+        {
+            Id = 1,
+            Content = "Test post",
+            AuthorId = user.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        Like like = new Like
+        {
+            PostId = post.Id,
+            UserId = user.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await this.dbContext.Users.AddAsync(user);
+        await this.dbContext.Posts.AddAsync(post);
+        await this.dbContext.Likes.AddAsync(like);
+        await this.dbContext.SaveChangesAsync();
+
+        var result = await this.likeService.ToggleLikeAsync(post.Id, user.Id);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result!.IsLiked, Is.False);
+        Assert.That(result.LikesCount, Is.EqualTo(0));
+        Assert.That(this.dbContext.Likes.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task ToggleLikeAsync_ShouldReturnNull_WhenPostDoesNotExist()
+    {
+        var result = await this.likeService.ToggleLikeAsync(999, "user-1");
+
+        Assert.That(result, Is.Null);
+    }
+
+    [Test]
+    public async Task ToggleLikeAsync_ShouldReturnNull_WhenPostIsDeleted()
+    {
+        ApplicationUser user = new ApplicationUser
+        {
+            Id = "user-1",
+            UserName = "user@test.com",
+            Email = "user@test.com",
+            DisplayName = "Test User"
+        };
+
+        Post deletedPost = new Post
+        {
+            Id = 1,
+            Content = "Deleted post",
+            AuthorId = user.Id,
+            CreatedOn = DateTime.UtcNow,
+            IsDeleted = true
+        };
+
+        await this.dbContext.Users.AddAsync(user);
+        await this.dbContext.Posts.AddAsync(deletedPost);
+        await this.dbContext.SaveChangesAsync();
+
+        var result = await this.likeService.ToggleLikeAsync(deletedPost.Id, user.Id);
+
+        Assert.That(result, Is.Null);
+        Assert.That(this.dbContext.Likes.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task IsPostLikedByUserAsync_ShouldReturnTrue_WhenUserLikedPost()
+    {
+        ApplicationUser user = new ApplicationUser
+        {
+            Id = "user-1",
+            UserName = "user@test.com",
+            Email = "user@test.com",
+            DisplayName = "Test User"
+        };
+
+        Post post = new Post
+        {
+            Id = 1,
+            Content = "Test post",
+            AuthorId = user.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        Like like = new Like
+        {
+            PostId = post.Id,
+            UserId = user.Id,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await this.dbContext.Users.AddAsync(user);
+        await this.dbContext.Posts.AddAsync(post);
+        await this.dbContext.Likes.AddAsync(like);
+        await this.dbContext.SaveChangesAsync();
+
+        bool result = await this.likeService.IsPostLikedByUserAsync(post.Id, user.Id);
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public async Task IsPostLikedByUserAsync_ShouldReturnFalse_WhenUserHasNotLikedPost()
+    {
+        bool result = await this.likeService.IsPostLikedByUserAsync(1, "user-1");
+
+        Assert.That(result, Is.False);
+    }
+}
