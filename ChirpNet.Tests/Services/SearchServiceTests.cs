2ea2de1a0aa274ea2a06a656158708822b653685
AntoniaDnv <antonia.d.neva@gmail.com>
2026-06-09 10:14:59 +0300

    ShouldFindUsersByDisplayName test added

--- ChirpNet.Tests/Services/SearchServiceTests.cs
+++ ChirpNet.Tests/Services/SearchServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using ChirpNet.Data.Data;
+using ChirpNet.Data.Data.Models;
 using ChirpNet.Services.Data.Services;
 using ChirpNet.Tests.Helpers;
 using NUnit.Framework;
@@ -34,6 +35,80 @@ namespace ChirpNet.Tests.Services
             Assert.That(result.Posts.Count, Is.EqualTo(0));
             Assert.That(result.Users.Count, Is.EqualTo(0));
         }
-       
+        [Test]
+
+        public async Task SearchAsync_ShouldFindPostByContent()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                Id = "user-1",
+                UserName = "maria@test.com",
+                Email = "maria@test.com",
+                DisplayName = "Maria"
+            };
+
+            await this.dbContext.Users.AddAsync(user);
+            await this.dbContext.Posts.AddRangeAsync(
+            new Post
+            {
+                Id = 1,
+                Content = "Learning ASP.NET MVC is fun",
+                AuthorId = user.Id,
+                CreatedOn = DateTime.UtcNow
+            },
+            new Post
+            {
+                Id = 2,
+                Content = "Completely unrelated text",
+                AuthorId = user.Id,
+                CreatedOn = DateTime.UtcNow
+            });
+            await this.dbContext.SaveChangesAsync();
+            var result = await this.searchService.SearchAsync("mvc");
+
+            Assert.That(result.Posts.Count(), Is.EqualTo(1));
+            Assert.That(result.Posts.First().Content, Does.Contain("MVC"));
+        }
+        [Test]
+        public async Task SearchAsync_ShouldNotReturnDeletedPosts()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                Id = "user-1",
+                UserName = "maria@test.com",
+                Email = "maria@test.com",
+                DisplayName = "Maria"
+            };
+            await this.dbContext.AddAsync(user);
+            await this.dbContext.Posts.AddAsync(new Post
+            {
+                Id = 1,
+                Content = "Hidden MVC post",
+                AuthorId = user.Id,
+                CreatedOn = DateTime.UtcNow,
+                IsDeleted = true
+            });
+            await this.dbContext.SaveChangesAsync();
+            var result = await this.searchService.SearchAsync("mvc");
+            Assert.That(result.Posts.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task SearchAsync_ShouldFindUsersByDisplayName()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                Id = "user-1",
+                UserName = "maria@test.com",
+                Email = "maria@test.com",
+                DisplayName = "Maria"
+            };
+            await this.dbContext.Users.AddAsync(user);
+            await this.dbContext.SaveChangesAsync();
+
+            var result = await this.searchService.SearchAsync("maria");
+            Assert.That(result.Users.Count(), Is.EqualTo(1));
+            Assert.That(result.Users.First().DisplayName, Is.EqualTo("Maria"));
+        }
     }
 }
